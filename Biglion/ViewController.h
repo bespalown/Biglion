aa85053310b7c4f566c4867dedeb8cde5d7e51d4
Viktor Bespalov <bespalown@gmail.com>
2014-07-09 21:58:01 +0400

    Initial Commit

---
+++ Biglion/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Biglion
+//
+//  Created by Viktor Bespalov on 09/07/14.
+//  Copyright (c) 2014 bespalown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BiglionTests/BiglionTests.m
@@ -0,0 +1,34 @@
+//
+//  BiglionTests.m
+//  BiglionTests
+//
+//  Created by Viktor Bespalov on 09/07/14.
+//  Copyright (c) 2014 bespalown. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BiglionTests : XCTestCase
+
+@end
+
+@implementation BiglionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
